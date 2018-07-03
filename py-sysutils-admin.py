08cbbf9faacb7b3f2cd8ab4a4542088b26a9ebb4
awongCM <awong.cm@gmail.com>
2018-07-03 15:23:08 +1000

    made better use of psutil class

--- py-sysutils-admin.py
+++ py-sysutils-admin.py
@@ -1,7 +1,18 @@
 # My first sysadmin memory monitoring usage!
+import os 
 import psutil
 
-# CPU percentage
-print(psutil.cpu_percent())
-# Virtual Memory
-print(psutil.virtual_memory())
+for proc in psutil.process_iter():
+  try:
+    pinfo = {
+      "pid": proc.ppid(),
+      "name": proc.name(),
+      "username": proc.username(),
+      "create_time": proc.create_time(),
+      "cpu_times": proc.cpu_times(),
+      "memory_usage_percent": proc.memory_percent()
+    }
+  except psutil.NoSuchProcess:
+    pass
+  else:
+    print(pinfo)
