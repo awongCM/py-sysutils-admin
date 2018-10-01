4b249e2dcf5c68583aefdf52d75eafef5714be19
awongCM <awong.cm@gmail.com>
2018-10-01 14:52:00 +1000

    added network and memory usage monitoring options

--- py-sysutils-admin.py
+++ py-sysutils-admin.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import os 
 from psutil import(cpu_percent, virtual_memory, __version__, process_iter)
+import psutil
+import time
 
 psutil_version = __version__
 cpu_percent = cpu_percent()
@@ -16,17 +18,60 @@ print("#-------------------#")
 print("#-------------------#")
 print('Memory (in GB): ', system_memory_in_gb)
 
-for proc in process_iter():
-  try:
-    pinfo = {
-      "pid": proc.ppid(),
-      "name": proc.name(),
-      "username": proc.username(),
-      "create_time": proc.create_time(),
-      "cpu_times": proc.cpu_times(),
-      "memory_usage_percent": proc.memory_percent()
-    }
-  except psutil.NoSuchProcess:
-    pass
+# Process Monitor
+def process_monitor():
+  for proc in process_iter():
+    try:
+      pinfo = {
+        "pid": proc.ppid(),
+        "name": proc.name(),
+        "username": proc.username(),
+        "create_time": proc.create_time(),
+        "cpu_times": proc.cpu_times(),
+        "memory_usage_percent": proc.memory_percent()
+      }
+    except psutil.NoSuchProcess:
+      pass
+    else:
+      print(pinfo)
+
+# Network monitor
+def network_monitor():
+  
+  old_value = 0
+
+  while True:
+    new_value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+
+    if old_value:
+      sent_sat(new_value - old_value)
+
+    old_value = new_value
+
+    time.sleep(1)
+
+
+def convert_to_gbit(value):
+  return value/1024./1024./1024.*8      
+
+def sent_sat(value):
+  print ("%0.3f" % convert_to_gbit(value))
+
+
+# Main Program starts
+def main():
+  print("Available options")
+  print("======= c - cpu and memory =========")
+  print("======= n - network  =========")
+  print("======= d - disk(TODO) =========")
+
+  monitor_type = input("Enter your monitoring type: ")
+
+  if monitor_type == 'c':
+    process_monitor()
+  elif monitor_type == 'n':
+    network_monitor()
   else:
-    print(pinfo)
+    print('Program ends here')
+
+main()
